01d9b16999ece2d6a3bfb3a5e5ff8c15689e77dc
litextension <litextension@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-12 10:42:01 +0000

    Wordpress Migration Version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/litextension-magento-to-woocommerce-migration/trunk@1219000 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== WordPress Migration ===
+Contributors: LitExtension
+Donate link: 
+Tags: migration, wordpress, woocommerce
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 0.6.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Simply the best migration plugin for WooCommerce, 
+help you migrate your products, customers, passwords, orders, categories, reviews,… 
+from other ecommerce to WooCommerce in a few clicks! 
+
+== Installation ==
+
+1. Upload the `litextension` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the Plugins -> WordPress Migration
+
---
+++ views/index.php
@@ -0,0 +1,36 @@
+<div class="wrap no-js">
+    <div style="width:1100px; font-family:Arial, Tahoma; font-size:15px; line-height:25px; padding-left:20px; padding-top:20px">
+        <div style="background-color:#000; height:130px; ">
+            <div style="padding:30px 50px 20px 30px; float: left"><a href="http://litextension.com"><img src="<?php echo wm_get_assets_url('img/litextension_logo.png') ?>" /></a></div>
+            <div style="color:#FFF; padding-right:40px; padding-top:30px">Magento to WooCommerce Migration Plugin is ultimate Shopping Cart Migration Solution by LitExtension. It allows you to migrate all your products, categories, orders, customers, passwords, SEO URLs and other data from Magento to WooCommerce in 3 simple steps.</div>
+        </div>
+        <center><div style="padding-top:40px">
+                <a href="http://litextension.com/woocommerce-migration-tool/magento-to-woocommerce.html"><img src="<?php echo wm_get_assets_url('img/magento_woocommerce.png') ?>" /></a>
+
+            </div></center>
+        <div id="main-slider" class="liquid-slider">
+            <div>
+                <h2 class="title">Connector Setup</h2>
+                <p>
+                    You need to download and copy Migration Connector into your Magento store, Please <a href="http://demo3.litextension.com/woocommerce_connector.zip">"Download It Here"</a>. After finished please make sure Migration Connector can be reached at: http(s)://yourstore.com/woocommerce_connector/connector.php. <br/><br/>
+                    For security, please open connector.php, find this very first line: define('LECM_TOKEN', '123456');<br/><br/>
+                    And change "123456" to another string, this will be used to enter to form below ( Cart Token ) and act like "password" to prevent unauthorized data access to your store.
+                </p>
+            </div>
+            <div>
+                <h2 class="title">Start Migration</h2>
+                <p>Good job. You are ready to start your migration now! <br/><br/>You will be redirected to LitExtension site to get plugin and start migration or you should try demo first.</p><br/>
+                <center><p><a href="http://demo3.litextension.com/woocommerce_target1/wp-admin/admin.php?page=cart-migration" target="_blank"><img src="<?php echo wm_get_assets_url('img/live_demo.png') ?>"/></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://litextension.com/woocommerce-migration-tool/magento-to-woocommerce.html" target="_blank"><img src="<?php echo wm_get_assets_url('img/get_plugin.png') ?>"/></a></p></center>
+            </div>          
+
+        </div><br><br>
+        <div style="background-color:#ee4b03; height:50px; color:#FFF; padding-top:20px; text-align: center; font-size:17px ">
+            If your have any question please feel free <a href="http://litextension.com/contacts/" target="_blank">Contact Us</a>. We look forward to assisting you.
+        </div>
+    </div>
+</div>	
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        jQuery('#main-slider').liquidSlider();
+    })
+</script>	
